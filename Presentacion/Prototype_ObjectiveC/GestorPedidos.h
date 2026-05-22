33628f2a0c0ff8b83ffc995b30eb0b0f8c580f40
Jorge iran Perez Segovia <144984581+IR1728@users.noreply.github.com>
2026-05-21 23:31:21 -0600

    Delete Presentacion/Prototype_ObjectiveC directory

--- Presentacion/Prototype_ObjectiveC/GestorPedidos.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface GestorPedidos : NSObject 
-
-- (void)hacerPedido;
-
-@end
--- Presentacion/Prototype_ObjectiveC/PrototipoPizza.h
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@protocol PrototipoPizza
-- (id)clonar;
-@end
